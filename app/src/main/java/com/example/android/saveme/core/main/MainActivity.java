4e9c14c0603f0f07919c27b98a7eec7644a6ad25
Harshika <hrshika.arya@gmail.com>
2018-03-29 19:33:03 +0530

    feat: successfully fetch user's location

--- app/src/main/java/com/example/android/saveme/core/main/MainActivity.java
+++ app/src/main/java/com/example/android/saveme/core/main/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.saveme.core.main;
 import android.Manifest;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -15,17 +16,18 @@ import android.widget.Toast;
 import com.example.android.saveme.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        ActivityCompat.OnRequestPermissionsResultCallback {
+public class MainActivity extends AppCompatActivity {
 
-    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_LOCATION = 1;
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private MainActivityViewModel mainActivityViewModel;
-    private GoogleApiClient googleApiClient;
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +36,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
         mainActivityViewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         getPermissions();
-
     }
 
     private void getPermissions() {
@@ -47,7 +49,9 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
                             Manifest.permission.ACCESS_COARSE_LOCATION},
-                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+                    MY_PERMISSIONS_REQUEST_ACCESS_LOCATION);
+        } else {
+            fetchUserLocation();
         }
     }
 
@@ -55,12 +59,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
         switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+            case MY_PERMISSIONS_REQUEST_ACCESS_LOCATION: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    fetchNearbyHospitals();
+                    fetchUserLocation();
                 } else {
 
                     // permission denied, boo! Disable the
@@ -71,36 +74,26 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
                     //TODO: Close the app
                 }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request.
+            } break;
+            default:
+                // no implementation
         }
     }
 
-    private void fetchNearbyHospitals() {
-        googleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API).build();
+    private void fetchUserLocation() {
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            fetchNearbyHospitals(location);
+                        }
+                    }
+                });
     }
 
-    // Play Services methods
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        // getting location
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        googleApiClient.connect();
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = "+ connectionResult.getErrorCode());
+    private void fetchNearbyHospitals(Location location) {
+        Log.v(TAG, location.getAltitude() + "");
     }
 }
 
--- app/src/main/java/com/example/android/saveme/core/main/MainActivityViewModel.java
+++ app/src/main/java/com/example/android/saveme/core/main/MainActivityViewModel.java
@@ -8,7 +8,5 @@ public class MainActivityViewModel extends ViewModel {
 
     }
 
-    private void fetchUserLocation() {
 
-    }
 }
