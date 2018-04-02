1313f54b078ae5e897c8cae3c09084f32f5f2c48
Harshika <hrshika.arya@gmail.com>
2018-04-02 20:36:16 +0530

    feat: Improve ui of the app (#8)

--- app/src/main/java/com/example/android/saveme/core/main/recycler_view/HospitalsAdapter.java
+++ app/src/main/java/com/example/android/saveme/core/main/recycler_view/HospitalsAdapter.java
@@ -1,14 +1,17 @@
 package com.example.android.saveme.core.main.recycler_view;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.android.saveme.R;
 import com.example.android.saveme.common.data.pojo.Result;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -23,9 +26,11 @@ import butterknife.ButterKnife;
 public class HospitalsAdapter extends RecyclerView.Adapter<HospitalsAdapter.ViewHolder> {
 
     private List<Result> hospitals;
+    private Context context;
 
-    public HospitalsAdapter(List<Result> hospitals) {
+    public HospitalsAdapter(List<Result> hospitals, Context context) {
         this.hospitals = hospitals;
+        this.context = context;
     }
 
     @NonNull
@@ -38,7 +43,7 @@ public class HospitalsAdapter extends RecyclerView.Adapter<HospitalsAdapter.View
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.bind(hospitals.get(position));
+        holder.bind(hospitals.get(position), context);
     }
 
     @Override
@@ -54,14 +59,47 @@ public class HospitalsAdapter extends RecyclerView.Adapter<HospitalsAdapter.View
         @BindView(R.id.tv_vicinity)
         public TextView vicinity;
 
+        @BindView(R.id.tv_rating)
+        public TextView rating;
+
+        @BindView(R.id.iv_status)
+        public ImageView status;
+
+        @BindView(R.id.iv_icon)
+        public ImageView icon;
+
         public ViewHolder(View view) {
             super(view);
             ButterKnife.bind(this, view);
         }
 
-        private void bind(Result hospital) {
-            name.setText(hospital.name);
-            vicinity.setText(hospital.vicinity);
+        private void bind(Result hospital, Context context) {
+            if (hospital.name != null) {
+                name.setText(hospital.name);
+            }
+
+            if (hospital.vicinity != null) {
+                vicinity.setText(hospital.vicinity);
+            }
+
+            if (hospital.icon != null) {
+                Picasso.with(context)
+                        .load(hospital.icon)
+                        .placeholder(R.mipmap.ic_launcher)
+                        .into(icon);
+            }
+
+            if (hospital.openingHours != null && hospital.openingHours.openNow != null) {
+                if(hospital.openingHours.openNow) {
+                    status.setImageResource(R.drawable.ic_lock_open_black_24dp);
+                } else {
+                    status.setImageResource(R.drawable.ic_lock_black_24dp);
+                }
+            }
+
+            if (hospital.rating != null) {
+                rating.setText(hospital.rating.toString());
+            }
         }
     }
 }
